f43091935ce92ba67f571047a16f00b3d8df3fa8
Daniel Silva Sales <daniel-silvasales@hotmail.com>
2024-01-09 00:52:53 -0300

    Jogo da Memória Basico Completo

---
+++ src/scripts/engine.js
@@ -0,0 +1,67 @@
+const emoji = [
+    "🐱‍👓",
+    "🐱‍👓",
+    "🐺",
+    "🐺",
+    "🦄",
+    "🦄",
+    "🤖",
+    "🤖",
+    "🦨",
+    "🦨",
+    "🐢",
+    "🐢",
+    "🐳",
+    "🐳",
+    "🦇",
+    "🦇"
+];
+
+let openCards = [];
+
+
+let shuffleEmojis = emoji.sort( () => 
+    Math.random() > 0.5 ? 2 : -1
+);
+
+
+for( let i = 0; i < emoji.length ; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").
+    appendChild(box);
+}
+
+
+function handleClick(){
+    if(openCards.length < 2 ){
+        this.classList.add("boxOpen");
+        openCards.push(this)
+    };
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    };
+}
+
+function checkMatch () {
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add
+        ("boxMatch")
+        openCards[1].classList.add
+        ("boxMatch")
+    }else {
+        openCards[0].classList.remove
+        ("boxOpen")
+        openCards[1].classList.remove
+        ("boxOpen")
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emoji.length){
+      alert("Você Vênceu")  
+    }
+};
